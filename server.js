d9709da7fed6ee202e1a60dd5618d0fc20696c9d
Jarvis <jarvis@workoutmeals.com.au>
2026-07-14 11:55:53 +1000

    Fix: Add PUT,DELETE to CORS allow-methods header (schedule updates were blocked)

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
   if (req.path.startsWith('/api/auth')) return next();
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   if (req.method === 'OPTIONS') return res.sendStatus(200);
   next();
